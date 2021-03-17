781ddfd301f2940ee4795cb8951716ebae7b246f
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2021-03-18 00:45:57 +0700

    Browser-side Error

--- src/components/survey/UserExperiences.vue
+++ src/components/survey/UserExperiences.vue
@@ -6,8 +6,9 @@
         <base-button @click="loadExperiences">Load Submitted Experiences</base-button>
       </div>
       <p v-if="isLoading">Loading...</p>
+      <p v-else-if="!isLoading && error">{{ error }}</p>
       <p v-else-if="!isLoading && (!results || results.length === 0)">No stored experiences found. Start adding some survey results first.</p>
-      <ul v-else-if="!isLoading && results && results.length > 0">
+      <ul v-else>
         <survey-result
           v-for="result in results"
           :key="result.id"
@@ -29,12 +30,14 @@ export default {
   data() {
     return {
       results: [],
-      isLoading: false
+      isLoading: false,
+      error: null
     }
   },
   methods: {
     loadExperiences() {
       this.isLoading = true;
+      this.error = null;
 
       fetch('https://vue-http-demo-86692-default-rtdb.firebaseio.com/surveys.json')
         .then(response => {
@@ -56,6 +59,11 @@ export default {
           }
 
           this.results = results;
+        })
+        .catch((error) => {
+          console.log(error);
+          this.isLoading = false;
+          this.error = 'Failed to fetch data, please try again later.';
         });
     }
   },
